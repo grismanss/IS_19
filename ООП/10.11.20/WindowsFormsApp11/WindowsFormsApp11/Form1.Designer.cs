7a4e488cc9850e4ae233f723253469ae9b426183
Your Name <you@example.com>
2020-11-10 10:07:34 +0700

    10/11/20

---
+++ ООП/10.11.20/WindowsFormsApp11/WindowsFormsApp11/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WindowsFormsApp11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(34, 31);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(520, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(520, 122);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(520, 148);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown2.TabIndex = 3;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(520, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(520, 182);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            400,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown3.TabIndex = 5;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            150,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+    }
+}
+
---
+++ ООП/10.11.20/WindowsFormsApp11/WindowsFormsApp11/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Graphics класс для работы с графикой
+            //Соединяем пикчебокс  обьектоном, на котором будем рисовать
+            Graphics polotno = Graphics.FromImage(pictureBox1.Image);
+            //Объект для рисования контура
+            Pen kontur = new Pen(Color.Red);
+            //Обьект для рисования заливки
+            SolidBrush kist = new SolidBrush(Color.Yellow);
+
+            //Закрашеный эллипс, передаем фон, х у ширину и высоту
+            polotno.FillEllipse(kist, 200, 150, 50, 90);
+            //Незакрашеный эллипс, передаем фон, х у ширину и высоту
+            polotno.DrawEllipse(kontur, 200, 150, 50, 90);
+
+            //Point класс для хранения точки с координатами х у
+            Point t = new Point(10, 20);
+            Point t2 = new Point(10, 200);
+            polotno.DrawLine(kontur, t, t2);//Построение линии по двум обьектам point
+
+            //Построение линии по 4м значениям х1 у1 х2 у2
+            polotno.DrawLine(kontur, 50,90,80,200);
+
+            //Рисованире прямоугодльного обькта
+            polotno.FillRectangle(kist,150, 200, 60, 90);
+            //Обновляем изображение в пикчебокс
+            pictureBox1.Refresh();
+
+            
+                
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Создаем программно изображение в пикчебокс
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Point t = new Point(Convert.ToInt32(numericUpDown1.Value),
+               Convert.ToInt32(numericUpDown2.Value));
+            int l = Convert.ToInt32(numericUpDown3.Value);
+
+            SolidBrush br = new SolidBrush(Color.Yellow);
+            SolidBrush br2 = new SolidBrush(Color.White);
+            SolidBrush br3 = new SolidBrush(Color.Black);
+
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            g.FillRectangle(br2, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            
+            //Лицо
+            g.FillEllipse(br, t.X, t.Y, l, l);
+
+            int l2 = l / 4;
+            Point t2 = new Point(t.X +l2, t.Y+ l2);
+
+            //пЕРВЫЙ ГЛАЗ
+            g.FillEllipse(br3, t2.X, t2.Y, l2-l2/2, l2- l2 / 2);
+            //вТОРОЙ ГЛАЗ
+            g.FillEllipse(br3, t.X+l-l2-(l2 - l2 / 2), t2.Y, l2 - l2 / 2, l2 - l2 / 2);
+
+            SolidBrush br4 = new SolidBrush(Color.Red);
+            g.FillRectangle(br4, t2.X, t.Y +l- l2, l - l2 - l2, l / 10);
+
+            pictureBox1.Refresh();
+        }
+    }
+}
